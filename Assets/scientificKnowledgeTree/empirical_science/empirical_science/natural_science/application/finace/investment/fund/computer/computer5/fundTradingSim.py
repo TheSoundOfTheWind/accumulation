333ed987e29abb1d6e43e27d9a69fa7882e454fc
TheSoundOfTheWind <18270949493@163.com>
2021-08-13 15:43:33 -0500

    add file

--- Assets/scientificKnowledgeTree/empirical_science/empirical_science/natural_science/application/finace/investment/fund/computer/computer5/fundTradingSim.py
+++ Assets/scientificKnowledgeTree/empirical_science/empirical_science/natural_science/application/finace/investment/fund/computer/computer5/fundTradingSim.py
@@ -17,38 +17,25 @@ def fundHightLow(filename, id):
 print("//----------------------------------------------------------------")
 id  = input("fund id:")
 list = fundHightLow("../../buy_fund/混合基金.txt", id)
-print("基金信息：%s %s %s %s" % (list[0], list[1], list[2], list[3]))
+print("id %s" % list[0])
+print("name %s" % list[1])
+print("volatility %s" % list[2])
+print("sharpe rate %s" % list[3])
+print("maximum drawdown %s" % list[4])
+print("maximum fund equity %s" % list[5])
+print("unit principal %s" % list[7])
+A = float(list[7])
+B = float(list[5])
+C = input("current cumulative net worth:")
+C = float(C)
+D = float(list[4])/100.0
+F = float(list[3])
+X = (B-C)/(B*D)
 
-l_p = float(list[4])
-h_p = float(list[5])
-c_p = input("current cumulative net worth:")
-c_p = float(c_p)
-e_v = input("estimate range:")
-e_v = float(e_v)
-if (h_p < c_p):
-    h_p = c_p
-elif (c_p < l_p):
-    l_p = c_p
-year=int((parser.parse(list[7]).year))
-month=int((parser.parse(list[7]).month))
-day=int((parser.parse(list[7]).day))
-d1 = datetime.datetime(year, month, day)
-d2 = datetime.datetime.now()
-years = float((d2-d1).days)/365.0
-
-e_y = float((h_p-l_p)/years)
-print("years %.2f e_y %.3f" % (years, e_y))
-e_y2 =e_y
-if(float(list[6]) > e_y):
-    e_y2=float(list[6])
-x=(h_p-c_p)/(e_y2*1.2)*10.0 - (e_v*0.01)
-b=2.0
-y=math.pow(b, x)
-print("x %.2f y %.2f" % (x,y))
-principal = float(list[10])
-principal = principal*(e_y/float(list[6]))
-investment_amount=principal*y
-down = (h_p-c_p)/e_y
-print("principal %.2f d_v %.3f down %.3f" % (principal, (h_p-c_p), down))    
-print ("suggested single investment amount: %.2f " % investment_amount)
-
+print("down %f" % X)
+X = (X*10)/2.0
+print("index %f" % X)
+X = (math.pow(2.0, X))
+print("multiple %f" % X)
+Y = A*F*X
+print ("suggested single investment amount: %.2f " % Y)
