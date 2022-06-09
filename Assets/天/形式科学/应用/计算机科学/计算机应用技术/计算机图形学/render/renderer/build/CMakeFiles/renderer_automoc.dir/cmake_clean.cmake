d6ae56ce444ab5e8bdff52e963572dd47e59aedf
TheSoundOfTheWind <18270949493@163.com>
2022-06-09 18:56:21 +0800

    add file

---
+++ Assets/天/形式科学/应用/计算机科学/计算机应用技术/计算机图形学/render/renderer/build/CMakeFiles/renderer_automoc.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "renderer_automoc.cpp"
+  "CMakeFiles/renderer_automoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/renderer_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
